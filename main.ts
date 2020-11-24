7fbece6d8404172ccba6c762e51763150ceeb105
Joao Nascimento <joao.nascimento@sensedia.com>
2020-11-23 21:13:26 -0300

    WIP: Lamport

---
+++ main.ts
@@ -0,0 +1,14 @@
+console.log('it works!');
+
+
+
+//ou é evento local
+
+//ou é comunicação com algum processo
+
+//isso é 
+
+var processId : String = '';
+var hostIP : String = '';
+var hostPort : String = '';
+var probability : Number = 0;
